4967b2baebee1a40e8b1c338650a50125958caac
claire wang <hellowang@foxmail.com>
2016-02-27 20:41:55 -0500

    gg

--- 133. Clone Graph.cc
+++ 133. Clone Graph.cc
@@ -6,6 +6,9 @@
  *     UndirectedGraphNode(int x) : label(x) {};
  * };
  */
+#include"../CC/header.h"
+
+
 class Solution {
 public:
     map<UndirectedGraphNode *, UndirectedGraphNode *>hash;//store the nodes
@@ -22,5 +25,42 @@ public:
         }
         //return cloned node
         return hash[node];
+    }
+};
+
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {//BFS
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(node==NULL)return NULL;
+        
+        map<UndirectedGraphNode *, UndirectedGraphNode *>mymap;//node: cloned node
+        queue<UndirectedGraphNode *>q;//q of current layer
+        
+        UndirectedGraphNode *newnode = new UndirectedGraphNode(node->label);
+        mymap[node] = newnode;
+        q.push(node);//node to clone
+        
+        while(!q.empty()){
+            UndirectedGraphNode *p = q.front(); q.pop();
+            for(auto n:p->neighbors){//copy neighbors
+                if(mymap.find(n) == mymap.end()){
+                    UndirectedGraphNode *np = new UndirectedGraphNode(n->label);
+                    mymap[n] = np;
+                    q.push(n);
+                }
+                //copy the neighbors
+                mymap[p]->neighbors.push_back(mymap[n]);
+            }
+        }
+        return mymap[node];
     }
 };
